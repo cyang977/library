fa1dc4d16e484a7b8481ed472a9133730b3dd389
Chang Yang <cy71997@yahoo.com>
2023-04-25 13:43:53 -0500

    Finished addToLibrary function

--- script/script.js
+++ script/script.js
@@ -1,5 +1,24 @@
 let myLibrary = [];
 
+let userInput = []; 
+let newBook;
+const titleBook = document.querySelector('#title');
+const authorName = document.querySelector('#author');
+const yearPublished = document.querySelector('#year');
+const maxPages = document.querySelector('#pages');
+
+
+const addBook = document.querySelector('.new-book-btn');
+addBook.addEventListener('click', () => {
+    console.log(titleBook.value);
+    console.log(authorName.value);
+    console.log(yearPublished.value);
+    console.log(maxPages.value);
+    newBook = new Book(titleBook.value, authorName.value, yearPublished.value, maxPages.value);
+    console.log(newBook);
+    addBookToLibrary(newBook); 
+})
+
 function Book(author, title, year, pages) {
     this.author = author;
     this.title = title;
@@ -7,6 +26,7 @@ function Book(author, title, year, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    console.log(myLibrary);
 }
