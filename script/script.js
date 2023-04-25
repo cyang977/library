c9a8dc7ab1661615c57ade500eeeb7b2207470cc
Chang Yang <cy71997@yahoo.com>
2023-04-25 10:34:10 -0500

    Empty functions and objects made

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, year, pages) {
+    this.author = author;
+    this.title = title;
+    this.year = year;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+
+}
