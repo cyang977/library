efa4b40ce77367c057069806e0b6e5e8474f90e6
Chang Yang <cy71997@yahoo.com>
2023-04-25 18:30:11 -0500

    Added functions of viewing books

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
 let myLibrary = [];
 
-let userInput = []; 
+let userInput = [];
+let newDiv;
+let divContent; 
+let emptyString;
 let newBook;
+let deleteBtn; 
+let pressDelete;
 const titleBook = document.querySelector('#title');
 const authorName = document.querySelector('#author');
 const yearPublished = document.querySelector('#year');
 const maxPages = document.querySelector('#pages');
+const booksGrid = document.querySelector('.books-grid');
 
 
 const addBook = document.querySelector('.new-book-btn');
@@ -19,7 +25,7 @@ addBook.addEventListener('click', () => {
     addBookToLibrary(newBook); 
 })
 
-function Book(author, title, year, pages) {
+function Book(title, author, year, pages) {
     this.author = author;
     this.title = title;
     this.year = year;
@@ -30,3 +36,41 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
     console.log(myLibrary);
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function deleteBook() {
+
+}
+
+const viewBook = document.querySelector('.view-btn');
+viewBook.addEventListener('click', () => {
+    removeAllChildNodes(booksGrid);
+    for (i of myLibrary){
+        console.log(i);
+        newDiv = document.createElement('div');
+        booksGrid.appendChild(newDiv);
+        divContent = document.createElement('p');
+        newDiv.appendChild(divContent);
+        emptyString = `Title: ${i.author}<br> Author: ${i.title}<br>
+        Year: ${i.year}<br> Pages: ${i.pages}`;
+        divContent.innerHTML = emptyString;
+        deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('deleteBtn');
+        deleteBtn.innerHTML = 'Delete';
+        pressDelete = document.querySelectorAll('.deleteBtn');
+        console.log(pressDelete);
+        // Need to figure out a way to delete certain books
+        pressDelete.forEach(btn => btn.addEventListener('click', () => {
+            console.log(e);
+            //deleteBook(); 
+        }));
+        newDiv.appendChild(deleteBtn);
+        divContent.classList.add('content');
+        newDiv.classList.add('view-books');
+    }
+})
